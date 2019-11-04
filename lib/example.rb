a0b3147865fe8dd152c64e7d62a9964225769d45
Kizito Njoku <nkizito57@gmail.com>
2019-11-04 15:33:50 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,11 +1,13 @@
 # Use this space to code along with the readme
 
-
-
-def greeting
-  puts "Hello World!"
-greeting
-greeting
-greeting
-greeting
-greeting
+phrase = "Hello World!"
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
